0d7a75c97befb4ae755a0b582095724feebfceed
aradharush <aradharush2013@gmail.com>
2025-08-25 19:19:05 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-blue-600">נטו שכר בע"מ</h1>
-    </div>
+<div className="flex items-center justify-center h-screen bg-gray-100 p-4">
+  <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-blue-600">
+     נטו שכר
+  </h1>
+</div>
+
   );
 }
 
