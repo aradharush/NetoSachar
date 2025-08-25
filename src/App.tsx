06bd62ba0daa11d379afccca57aecc5461b45da8
aradharush <aradharush2013@gmail.com>
2025-08-25 19:05:35 +0300

    change to bam

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-blue-600">נטו שכר</h1>
+      <h1 className="text-6xl font-bold text-blue-600">נטו שכר בע"מ</h1>
     </div>
   );
 }
